6a14d9297a284d792fc52577ef265e38a9e941ee
lasjg72 <lassjg28@gmail.com>
2021-03-02 03:18:37 +0900

    add for Musou3

---
+++ Pachi/HourlyWage/Musou3/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Spec_Musou3 sp3 = new Spec_Musou3();
+        sp3.NB_init();
+        sp3.DB_init();
+        sp3.Calc_dedama_per_kakuhen();
+        System.out.println("ラウンドベース込みの時給: " + Values.Calc_money_per_hour(sp3)*4);
+    }
+}
